a6df6045c60cc462a3bc4abdd67c56b3addaaabe
Fikri Miftah <fikri.droid16@gmail.com>
2022-02-02 12:19:52 +0700

    SQL Update After GSheets API Write Function Worked

--- washinngarage.sql
+++ washinngarage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2022 at 12:33 AM
+-- Generation Time: Feb 02, 2022 at 12:18 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
