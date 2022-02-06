defd193df95440bfeaa7b9ad322f257bfb8c77e4
Fikri Miftah <fikri.droid16@gmail.com>
2022-02-06 16:57:03 +0700

    link to index

--- operator/view-template/sidebar.php
+++ operator/view-template/sidebar.php
@@ -18,7 +18,7 @@ if ($activePageLvl == 0) {
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon">
             <img src="<?= $assets ?>/img/logo-white.png" width=60px>
         </div>
