0b85e6e324fd7c2029ba3e8cb402e8f65b3ac98a
Fikri Miftah <fikri.droid16@gmail.com>
2022-02-12 23:35:30 +0700

    Code Cleaning and Signing

--- admin/customer-vehicle.php
+++ admin/customer-vehicle.php
@@ -1,15 +1,11 @@
 <?php
 require_once 'admin-only.php';
 $activePageLvl = 3;
-
-// Get Data From DB For User List
 $query_getUser = "SELECT * FROM users WHERE username != '$current_user'";
 $execute_getUser = mysqli_query($link, $query_getUser);
 $i = 1;
-
 $query_getVehicles = "SELECT * FROM vehicles";
 $execute_getVehicles = mysqli_query($link, $query_getVehicles);
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,39 +14,19 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Manage Menu</title>
-
     <link rel="icon" type="image/png" href="<?= $assets ?>/img/logo.png">
     <link rel="stylesheet" type="text/css" href="<?= $assets ?>/css/sb-admin-2.min.css">
     <link href="<?= $assets ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-    <!-- dataTable css -->
     <link href="<?= $assets ?>/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-
-
-
 </head>
 
 <body class="page-top">
-
-    <!-- Page Wrapper -->
     <div id="wrapper">
-
-        <!-- Sidebar Attach -->
         <?php require_once 'view-template/sidebar.php'; ?>
-
-        <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
             <div id="content">
-
-                <!-- Topbar Attach -->
                 <?php require_once 'view-template/topbar.php'; ?>
-
-                <!-- Begin Page Content -->
                 <div class="container-fluid">
-
-                    <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Customer's Vehicle Data </h1>
                         <?php
@@ -121,36 +97,14 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
                             }
                         }
                         ?>
-
-                        <!-- <a target="_blank" href="download-builder/user-data-download.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Download User Data</a> -->
                     </div>
-
-                    <!-- Content Row -->
-
                     <div class="row">
-                        <!-- User Manager -->
                         <div class="col-xl-12 col-lg-12" id="panelUtama">
                             <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                     <h5 class="m-0 font-weight-bold text-primary"><i class="fas fa-smile fa-fw"></i> Customers Information</h5>
-                                    <!-- <a data-toggle="modal" data-target="#addMenuModal" class="btn btn-success"><i class="fas fa-plus fa-fw"></i> Add Menu</a> -->
-                                    <!-- <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div> -->
+
                                 </div>
-                                <!-- Card Body -->
                                 <div class="card-body">
                                     <table class="table table-striped" id="services">
                                         <thead>
@@ -176,27 +130,24 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
                                                     <td><?= ($vehicle['vehicletype'] == 'Mobil') ? 'Car' : 'Motorcycle' ?></td>
                                                     <td><?= $ownername ?></td>
                                                 </tr>
-                                            <?php $i++;
-                                            } ?>
+                                            <?php
+                                                $i++;
+                                            }
+                                            ?>
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                         </div>
-                        <!--  -->
                         <div class="col-xl-4 col-lg-4" hidden id="detailPanel">
                             <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                     <h5 class="m-0 font-weight-bold text-primary" id="rightPaneTitle"><i class="fas fa-edit fa-fw"></i> Edit Vehicle Detail</h5>
                                     <a class="btn btn-danger" onclick="closeDetailPanel()"><i class="fas fa-times fa-fw"></i></a>
-
                                 </div>
-
                                 <div class="card-body" id="modeEdit">
                                     <form action="functions/update-vehicle-data.php" method="post" enctype="multipart/form-data">
                                         <div class="form-row">
-
                                             <div class="form-group col-md-5">
                                                 <label for="updateCategory">Category</label>
                                                 <select id="updateCategory" class="form-control" name="category">
@@ -209,7 +160,6 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
                                                 <input required autocomplete="off" type="text" class="form-control" id="updatePlatnomor" placeholder="Reg. Number" name="platnomor">
                                             </div>
                                         </div>
-
                                         <div class="row">
                                             <div class="col-md-12 d-flex justify-content-between">
                                                 <a class="btn btn-secondary" onclick="closeDetailPanel()">Cancel</a> &emsp;
@@ -222,35 +172,15 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
-
-
-
                 </div>
-                <!-- /.container-fluid -->
-
             </div>
-            <!-- End of Main Content -->
-
-            <!-- Footer Attach -->
             <?php require_once "view-template/footer.php" ?>
-
         </div>
-        <!-- End of Content Wrapper -->
-
     </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
-
-
-
-
 
     <!-- Modal Add New Menu -->
     <div class="modal fade " data-backdrop="static" id="addMenuModal" tabindex="-1" aria-labelledby="addMenuModalLabel" aria-hidden="true">
@@ -303,10 +233,8 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
                             <div class="form-group col-md-12">
                                 <label for="inputServiceDesc">Service Description (Optional)</label>
                                 <textarea autocomplete="off" class="form-control" style="min-height: 100px; max-height: 200px;" placeholder="Description for this service" name="servicedesc"></textarea>
-
                             </div>
                         </div>
-
                         <div class="form-group">
                             <div class="custom-control custom-switch">
                                 <input type="checkbox" class="custom-control-input" id="setActiveCheck" name="activate">
@@ -323,7 +251,6 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
         </div>
     </div>
     <!-- Modal Add New Menu  END-->
-
 
     <!-- Modal confirm delete service -->
     <div class="modal fade" id="deleteservice" tabindex="-1" aria-labelledby="deleteserviceLabel" aria-hidden="true">
@@ -351,95 +278,89 @@ $execute_getVehicles = mysqli_query($link, $query_getVehicles);
         </div>
     </div>
     <!-- Modal confirm delete service END-->
-
-
-
-
-
-
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?= $assets ?>/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= $assets ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="<?= $assets ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?= $assets ?>/js/sb-admin-2.min.js"></script>
-
-    <!-- dataTable js -->
-    <script src="<?= $assets ?>/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?= $assets ?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#services').DataTable();
-        });
-
-
-
-
-
-        function imageSelected() {
-            if (document.getElementById("inputFileImage").value != '') {
-                var input = document.getElementById("inputFileImage");
-                document.getElementById("inputFileImageLabel").innerHTML = input.files.item(0).name;
-            }
-        }
-
-        function imageSelected2() {
-            if (document.getElementById("updateImage").value != '') {
-                var input = document.getElementById("updateImage");
-                document.getElementById("updateImageLabel").innerHTML = input.files.item(0).name;
-            }
-        }
-
-        function openDetail(ownername, vehicletype, platnomor, id) {
-            document.getElementById('detailPanel').hidden = false;
-            document.getElementById('modeEdit').hidden = false;
-
-            console.log(id);
-
-            // document.getElementById('thumbnailShow').src = '../assets/img/thumbnail/' + thumbnail;
-            document.getElementById('hiddenServiceID').value = id;
-            document.getElementById('serviceidhidden').value = id;
-            // document.getElementById('deleteTitle').innerHTML = '[ ' + fullname + ' ]';
-
-
-            document.getElementById('updateCategory').value = vehicletype;
-            document.getElementById('updatePlatnomor').value = platnomor;
-
-
-            document.getElementById('panelUtama').className = 'col-xl-8 col-lg-8';
-            if (status == 'Active') {
-                document.getElementById('statusShow').innerHTML = '<i class="fas fa-dot-circle text-info"></i> Active';
-            } else if (status == 'Inactive') {
-                document.getElementById('statusShow').innerHTML = '<i class="far fa-dot-circle text-danger"></i> Inactive';
-            }
-
-
-
-        }
-
-        function closeDetailPanel() {
-            document.getElementById('detailPanel').hidden = true;
-            document.getElementById('panelUtama').className = 'col-xl-12 col-lg-12';
-        }
-
-        function changeMode() {
-            if (document.getElementById('modeView').hidden == false) {
-                document.getElementById('modeView').hidden = true;
-                document.getElementById('modeEdit').hidden = false;
-                document.getElementById('rightPaneTitle').innerHTML = '<i class="fas fa-edit fa-fw"></i> Edit Service Detail';
-            } else {
-                document.getElementById('modeView').hidden = false;
-                document.getElementById('modeEdit').hidden = true;
-                document.getElementById('rightPaneTitle').innerHTML = '<i class="fas fa-info-circle fa-fw"></i> Service Detail';
-            }
-        }
-    </script>
-
 </body>
 
 </html>
+
+<script src="<?= $assets ?>/vendor/jquery/jquery.min.js"></script>
+<script src="<?= $assets ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= $assets ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?= $assets ?>/js/sb-admin-2.min.js"></script>
+<script src="<?= $assets ?>/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= $assets ?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#services').DataTable();
+    });
+
+    function imageSelected() {
+        if (document.getElementById("inputFileImage").value != '') {
+            var input = document.getElementById("inputFileImage");
+            document.getElementById("inputFileImageLabel").innerHTML = input.files.item(0).name;
+        }
+    }
+
+    function imageSelected2() {
+        if (document.getElementById("updateImage").value != '') {
+            var input = document.getElementById("updateImage");
+            document.getElementById("updateImageLabel").innerHTML = input.files.item(0).name;
+        }
+    }
+
+    function openDetail(ownername, vehicletype, platnomor, id) {
+        document.getElementById('detailPanel').hidden = false;
+        document.getElementById('modeEdit').hidden = false;
+        document.getElementById('hiddenServiceID').value = id;
+        document.getElementById('serviceidhidden').value = id;
+
+        document.getElementById('updateCategory').value = vehicletype;
+        document.getElementById('updatePlatnomor').value = platnomor;
+
+        document.getElementById('panelUtama').className = 'col-xl-8 col-lg-8';
+        if (status == 'Active') {
+            document.getElementById('statusShow').innerHTML = '<i class="fas fa-dot-circle text-info"></i> Active';
+        } else if (status == 'Inactive') {
+            document.getElementById('statusShow').innerHTML = '<i class="far fa-dot-circle text-danger"></i> Inactive';
+        }
+    }
+
+    function closeDetailPanel() {
+        document.getElementById('detailPanel').hidden = true;
+        document.getElementById('panelUtama').className = 'col-xl-12 col-lg-12';
+    }
+
+    function changeMode() {
+        if (document.getElementById('modeView').hidden == false) {
+            document.getElementById('modeView').hidden = true;
+            document.getElementById('modeEdit').hidden = false;
+            document.getElementById('rightPaneTitle').innerHTML = '<i class="fas fa-edit fa-fw"></i> Edit Service Detail';
+        } else {
+            document.getElementById('modeView').hidden = false;
+            document.getElementById('modeEdit').hidden = true;
+            document.getElementById('rightPaneTitle').innerHTML = '<i class="fas fa-info-circle fa-fw"></i> Service Detail';
+        }
+    }
+</script>
+
+<!-- ==============================================
+
+FFFFFFFFFFFFFFFFFFFFFFFFFF
+ FFFFFFFFFFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFFFFFFFFF
+  FFFFF                FFF
+  FFFFF
+  FFFFF         FFF
+  FFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFF
+  FFFFF         FFF
+  FFFFF
+  FFFFF
+  FFFFF
+  FFFFF
+  FFFFF
+ FFFFFFF
+FFFFFFFFF
+
+==============================================  -->
--- login.php
+++ login.php
@@ -35,16 +35,12 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- Style CSS -->
+    <title>Login - Wash Inn Garage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" type="text/css" href="assets/vendor/fontawesome-free/css/all.min.css" />
     <link rel="icon" type="image/png" href="assets/img/logo.png" />
-
-    <title>Login - Wash Inn Garage</title>
 </head>
 
 <body>
@@ -81,8 +77,6 @@ if (isset($_POST['submit'])) {
 </body>
 
 </html>
-
-
 
 <style type="text/css">
     /*
