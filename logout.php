2a024714d66c487dcb0ba17d91bab0c496a6768d
Fikri Miftah <fikri.droid16@gmail.com>
2022-02-12 21:13:12 +0700

    Code Cleaning and Signing

--- logout.php
+++ logout.php
@@ -1,6 +1,27 @@
-<?php 
-	require_once 'core/init.php';
+<?php
+require_once 'core/init.php';
+session_destroy();
+header("Location:login.php");
+?>
 
-	session_destroy();
-	header("Location:login.php");
- ?>
+<!-- ==============================================
+
+FFFFFFFFFFFFFFFFFFFFFFFFFF
+ FFFFFFFFFFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFFFFFFFFF
+  FFFFF                FFF
+  FFFFF
+  FFFFF         FFF
+  FFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFF
+  FFFFF         FFF
+  FFFFF
+  FFFFF
+  FFFFF
+  FFFFF
+  FFFFF
+ FFFFFFF
+FFFFFFFFF
+
+==============================================  -->
