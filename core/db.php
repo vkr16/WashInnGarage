ec3cf311158bdf6b77658a6e2270490cf045415d
Fikri Miftah <fikri.droid16@gmail.com>
2022-02-17 20:02:55 +0700

    Code Cleaning and Signing

--- core/db.php
+++ core/db.php
@@ -1,10 +1,29 @@
-<?php 
-
-$host		= "localhost";
-$user		= "root";
-$pass	 	= "";
-$db			= "washinngarage";
-
-$link = mysqli_connect($host,$user,$pass, $db) or die(mysqli_error);
-
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "washinngarage";
+$link = mysqli_connect($host, $user, $pass, $db) or die(mysqli_error);
 ?>
+
+<!-- ==============================================
+
+FFFFFFFFFFFFFFFFFFFFFFFFFF
+ FFFFFFFFFFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFFFFFFFFF
+  FFFFF                FFF
+  FFFFF
+  FFFFF         FFF
+  FFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFF
+  FFFFFFFFFFFFFFFFF
+  FFFFF         FFF
+  FFFFF
+  FFFFF
+  FFFFF
+  FFFFF
+  FFFFF
+ FFFFFFF
+FFFFFFFFF
+
+==============================================  -->
